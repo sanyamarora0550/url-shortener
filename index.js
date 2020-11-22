05e5fd28313b571001c9ec03b0aa4fac985cdc29
sanyam.a <sanyam.a@neodove.com>
2020-11-22 21:50:02 +0530

    heroku config

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json()); // req.body
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 connectDB();
 
 app.use('/', require('./routes/index'));
